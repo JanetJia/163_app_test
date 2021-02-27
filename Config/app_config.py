aed2c4eab82cdafcc4a8ca553dceee5a4bc2dadf
JanetJia <52478153+JanetJia@users.noreply.github.com>
2021-02-27 15:04:40 +0800

    app配置 联系人信息定位i

---
+++ Config/app_config.py
@@ -0,0 +1,15 @@
+# 邮箱大师app配置
+
+Desired_Capabilities = {
+  "platformName": "Android",
+  "platformVersion": "9.0",
+  "deviceName": "Pixel 2",
+  "automationName": "Appium",
+  "appPackage": "com.netease.mail",
+  "appActivity": "com.netease.mobimail.activity.TabActivity",
+  "noReset": True,
+  "unicodeKeyboard": True,
+  "resetKeyboard": True,
+  'skipServerInstallation': False,
+  'skipDeviceInitialization': False
+}
